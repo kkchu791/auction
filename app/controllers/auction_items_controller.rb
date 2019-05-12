e1289b3adb52877894b03e3a90cc3fce6bca86d9
Kirk <kkchu791@gmail.com>
2019-05-12 13:40:46 -0700

    Add basic bidding logic

--- app/controllers/auction_items_controller.rb
+++ app/controllers/auction_items_controller.rb
@@ -2,4 +2,28 @@ class AuctionItemsController < ApplicationController
   def index
     @auction_items = AuctionItem.all
   end
+
+  def show
+    @auction_item = AuctionItem.find(params[:id])
+  end
+
+  def edit
+    @auction_item = AuctionItem.find(params[:id])
+  end
+
+  def update
+    @auction_item = AuctionItem.find(params[:id])
+    if @auction_item.update_attributes(auction_item_params)
+      render 'success'
+    else
+      render 'edit'
+    end
+  end
+
+
+  private
+
+  def auction_item_params
+    params.require(:auction_item).permit(:highest_bid)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'auction_items#index'
+  # get '/auction_items/:id', to: 'auction_items#show'
+  # get '/auction_items/:id', to: 'auction_items#update'
+  resources :auction_items, only: [:index, :show, :edit, :update]
 end
