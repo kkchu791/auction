153d90b59033dc0d6b447f5892ec883e88c002a8
Kirk <kkchu791@gmail.com>
2019-05-15 00:43:03 -0700

    Remove some unnecessary files

--- spec/requests/auction_items_spec.rb
+++ spec/requests/auction_items_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 describe "auction_items", type: :request do
-
-  let(:user) { User.create(email: "test@gmail.com", password: "Password1!") }
-  let(:auction_item) { AuctionItem.create(name: "Fan") }
-
   describe "GET /auction_items" do
     it "should have an endpoint" do
       get '/auction_items'
@@ -14,30 +10,6 @@ describe "auction_items", type: :request do
     it "should render index template" do
       get '/auction_items'
       expect(response).to render_template(:index)
-    end
-  end
-
-  describe "SHOW /auction_items/:id" do
-    it "should have an endpoint" do
-      get "/auction_items/#{auction_item.id}"
-      expect(response).to have_http_status(200)
-    end
-
-    it "should render show template" do
-      get '/auction_items'
-      expect(response).to render_template(:show)
-    end
-  end
-
-  describe "PATCH /place_bid/:id" do
-    it "should have an endpoint" do
-      patch "/auction_items/#{auction_item.id}"
-      expect(response).to have_http_status(200)
-    end
-
-    it "should render show template" do
-      get '/'
-      expect(response).to render_template(:show)
     end
   end
 end
